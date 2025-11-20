6d7da624f0d4ca9f57ec7f733941f50512c1de0d
Vova <85615105+vova0020@users.noreply.github.com>
2025-11-20 03:38:16 +0300

    1

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -19,7 +19,7 @@ module.exports = function(app) {
   app.use(
     '/socket.io',
     createProxyMiddleware({
-      target: apiTargetWS,
+      target: apiTarget,
       changeOrigin: true,
       ws: true, // ОБЯЗАТЕЛЬНО для Socket.IO
     })
